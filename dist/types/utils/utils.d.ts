2a70a28bbd8d1d7191eb337ed831c605dfe6c61c
emnudge <emnudge@gmail.com>
2019-11-01 16:24:11 -0400

    removed compiled folders

--- dist/types/utils/utils.d.ts
+++
@@ -1,8 +0,0 @@
-export declare function getTimecode(seconds: number): string;
-export declare function getClass(...classes: any): any;
-interface Range {
-    min: number;
-    max: number;
-}
-export declare function mapRange(val: number, range1: Range, range2: Range): number;
-export {};
--- www/build/stencil-audioplayer.esm.js
+++
@@ -1,6 +0,0 @@
-import { p as patchBrowser, g as globals, b as bootstrapLazy } from './core-d79ee1c8.js';
-
-patchBrowser().then(options => {
-  globals();
-  return bootstrapLazy([["keyframe-editor",[[1,"keyframe-editor",{"open":[4],"name":[1],"isCollapsed":[32],"getHeightPercentage":[64]}]]],["keyframed-audio-player",[[1,"keyframed-audio-player",{"name":[1],"url":[1],"isPaused":[32],"currentTime":[32],"duration":[32],"audioFile":[32]}]]]], options);
-});
