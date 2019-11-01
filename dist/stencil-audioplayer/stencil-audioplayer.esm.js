b0e4a9555f3f72a4a044c12375c93bd0d12b1c28
emnudge <emnudge@gmail.com>
2019-11-01 16:19:09 -0400

    cleaned up index and removed compiled files

--- dist/stencil-audioplayer/stencil-audioplayer.esm.js
+++ dist/stencil-audioplayer/stencil-audioplayer.esm.js
@@ -1,1 +1,6 @@
-import{p as e,b as a}from"./p-905463c5.js";e().then(e=>a([["p-tjo8yo0g",[[1,"keyframed-audio-player",{name:[1],url:[1],isPaused:[32],currentTime:[32],duration:[32],audioFile:[32]}],[1,"keyframe-editor",{open:[4],name:[1],isCollapsed:[32],getHeightPercentage:[64]}]]]],e));
+import { p as patchBrowser, g as globals, b as bootstrapLazy } from './core-d79ee1c8.js';
+
+patchBrowser().then(options => {
+  globals();
+  return bootstrapLazy([["keyframe-editor",[[1,"keyframe-editor",{"open":[4],"name":[1],"isCollapsed":[32],"getHeightPercentage":[64]}]]],["keyframed-audio-player",[[1,"keyframed-audio-player",{"name":[1],"url":[1],"isPaused":[32],"currentTime":[32],"duration":[32],"audioFile":[32]}]]]], options);
+});
